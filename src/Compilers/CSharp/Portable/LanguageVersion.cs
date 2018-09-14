0a61b88f7cd1636d3d574ce531ea3ba65bc4dbe0
Alireza Habibi <habibi.alireza@outlook.com>
2018-09-14 16:23:44 +0430

    Extend eligible params types to array generic interfaces and span

--- src/Compilers/CSharp/Portable/LanguageVersion.cs
+++ src/Compilers/CSharp/Portable/LanguageVersion.cs
@@ -345,5 +345,10 @@ namespace Microsoft.CodeAnalysis.CSharp
         {
             return self >= MessageID.IDS_FeatureImprovedOverloadCandidates.RequiredVersion();
         }
+
+        internal static bool AllowParamsArrayInterfaceAndSpan(this LanguageVersion self)
+        {
+            return self >= MessageID.IDS_FeatureParamsArrayInterfaceAndSpan.RequiredVersion();
+        }
     }
 }
