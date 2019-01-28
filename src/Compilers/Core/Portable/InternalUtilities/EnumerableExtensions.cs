21584a0117908187ac7abb7f6b6698ae774ca7aa
Alireza Habibi <habibi.alireza@outlook.com>
2019-01-28 12:28:54 +0330

    Extract reducer to its own class

--- src/Compilers/Core/Portable/InternalUtilities/EnumerableExtensions.cs
+++ src/Compilers/Core/Portable/InternalUtilities/EnumerableExtensions.cs
@@ -368,6 +368,25 @@ namespace Roslyn.Utilities
         {
             return Comparer<T>.Create(comparison);
         }
+
+        public static T AggregateOrDefault<T>(this IEnumerable<T> source, Func<T, T, T> func)
+        {
+            using (var e = source.GetEnumerator())
+            {
+                if (!e.MoveNext())
+                {
+                    return default;
+                }
+
+                var result = e.Current;
+                while (e.MoveNext())
+                {
+                    result = func(result, e.Current);
+                }
+
+                return result;
+            }
+        }
     }
 
     /// <summary>
